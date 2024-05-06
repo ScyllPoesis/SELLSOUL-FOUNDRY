fb318d80776bf318a7464dc127379705a4d9c949
Kairos <ourodaiko@gmail.com>
2024-05-06 17:11:29 +0000

    fix v12 nonsense

--- module/data/weapon.mjs
+++ module/data/weapon.mjs
@@ -1,9 +1,8 @@
 export default class SellSoulWeaponProfile extends foundry.abstract.TypeDataModel {
-
   static defineSchema() {
     const fields = foundry.data.fields;
     const requiredInteger = { required: true, nullable: false, integer: true };
-    const schema = super.defineSchema();
+    const schema = {};
 
     // Whether this profile should be treated as a CORE WEAPON.
     schema.core = new fields.BooleanField({
@@ -20,10 +19,25 @@ export default class SellSoulWeaponProfile extends foundry.abstract.TypeDataMode
     });
 
     // Attribute fields.
-    schema.ap = new fields.NumberField({ ...requiredInteger, initial: 0 });
+    schema.ap = new fields.NumberField({ ...requiredInteger, initial: 0, label: "AP", hint: "Armor Penetration" });
     schema.sal = new fields.NumberField({ ...requiredInteger, initial: 0 });
     schema.ran = new fields.NumberField({ ...requiredInteger, initial: 1 });
 
     return schema;
+  }
+
+  prepareDerivedData() {
+    // Calculate current effective DAM.
+    for (const key in this.dam.slider) {
+      console.log(key);
+      this.dam.eff = this.dam.slider[key];
+    }
+  }
+
+  getRollData() {
+    const data = { ap: this.ap, sal: this.sal, ran: this.ran };
+    data[slider] = this.dam.slider;
+
+    return data;
   }
 }
