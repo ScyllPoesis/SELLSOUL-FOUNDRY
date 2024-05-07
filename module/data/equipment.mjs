5b1a6e9af52c02d9d9576838f02ca10cf66373ab
Kairos <ourodaiko@gmail.com>
2024-05-07 00:43:25 +0000

    amend

--- module/data/equipment.mjs
+++ module/data/equipment.mjs
@@ -4,17 +4,11 @@ export default class SellSoulEquipment extends foundry.abstract.TypeDataModel {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = {};
 
-    schema.quantity = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
-    schema.weight = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
+    schema.rules = new fields.StringField({ required: true, initial: "This unit is granted some boon or bane." });
+    schema.type = new fields.StringField({ required: true, initial: "unit" });
+    schema.description = new fields.StringField({ required: true, initial: "An assortment of characteristics describes this trait." });
 
-    // Break down roll formula into three independent fields
-    schema.roll = new fields.SchemaField({
-      diceNum: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 }),
-      diceSize: new fields.StringField({ initial: "d20" }),
-      diceBonus: new fields.StringField({ initial: "+@str.mod+ceil(@lvl / 2)" })
-    })
-
-    schema.formula = new fields.StringField({ blank: true });
+    schema.required
 
     return schema;
   }
